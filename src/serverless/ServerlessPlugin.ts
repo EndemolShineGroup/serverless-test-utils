13afc4bf9d66bfb1818eae904b39d9b84d52d9b4
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-05 05:22:47 +0000

    refactor(serverless): use types package

--- src/serverless/ServerlessPlugin.ts
+++ src/serverless/ServerlessPlugin.ts
@@ -1,6 +1,12 @@
+import Serverless from 'serverless';
+
 // mock to test functionality bound to a serverless plugin
 export default class ServerlessPlugin {
-  constructor(public serverless: any, public options: any, testSubject: any) {
+  constructor(
+    public serverless: Serverless,
+    public options: any,
+    testSubject: any,
+  ) {
     Object.assign(this, testSubject);
   }
 }
