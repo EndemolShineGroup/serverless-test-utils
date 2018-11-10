308369e5b83917803a67fb418a3beebdff1b3e44
Hassan Khan <hassan.khan@endemolshine.com>
2018-11-10 19:58:48 +0000

    fix(sns): add region parameter to helper functions

--- src/aws/sns/createSnsTopic.ts
+++ src/aws/sns/createSnsTopic.ts
@@ -1,7 +1,7 @@
 import AWS from 'aws-sdk';
 
-export default async (topicName: string) => {
-  const sns = new AWS.SNS();
+export default async (topicName: string, region = 'us-east-1') => {
+  const sns = new AWS.SNS({ region });
 
   const params = {
     Name: topicName,
--- src/aws/sns/publishSnsMessage.ts
+++ src/aws/sns/publishSnsMessage.ts
@@ -1,7 +1,11 @@
 import AWS from 'aws-sdk';
 
-export default async (topicName: string, message: string) => {
-  const sns = new AWS.SNS();
+export default async (
+  topicName: string,
+  message: string,
+  region = 'us-east-1',
+) => {
+  const sns = new AWS.SNS({ region });
 
   const listTopicsResponse = await sns.listTopics().promise();
 
